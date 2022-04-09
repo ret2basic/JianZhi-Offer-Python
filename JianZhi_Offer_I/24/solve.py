2173a4504d7993d86c2082c35182d0c932c56ef8
ret2basic <ret2basic@gmail.com>
2022-04-09 01:30:35 -0700

    24

---
+++ JianZhi_Offer_I/24/solve.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        previous_node = None
+        current_node = head
+        while current_node:
+            next_node = current_node.next
+            current_node.next = previous_node
+            previous_node = current_node
+            current_node = next_node
+        return previous_node
