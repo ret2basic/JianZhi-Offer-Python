1c9d33b9698b4aa3b3907b94e70295394d810fc8
ret2basic <ret2basic@gmail.com>
2022-04-09 23:28:40 -0700

    25

--- JianZhi_Offer_I/25/solve.py
+++ JianZhi_Offer_I/25/solve.py
@@ -6,14 +6,21 @@
 
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-        current_node = sentinel = ListNode(0)
+        current = sentinel = ListNode(0)
 
         while l1 and l2:
-            if l1.val < l2.val:
-                current_node.next, l1 = l1, l1.next
+            if l1.val <= l2.val:
+                current.next = l1
+                l1 = l1.next
             else:
-                current_node.next, l2 = l2, l2.next
-            current_node = current_node.next
-        current_node.next = l1 if l1 else l2
+                current.next = l2
+                l2 = l2.next
+
+            current = current.next
+
+        if l1:
+            current.next = l1
+        elif l2:
+            current.next = l2
         
         return sentinel.next
