65dbc720a7f78520d4eb1b257520396d2c119753
ret2basic <ret2basic@gmail.com>
2022-04-16 05:24:16 -0700

    32

---
+++ JianZhi_Offer_I/32/solve.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[int]:
+        if not root:
+            return []
+
+        result = []
+        queue = collections.deque()
+        queue.append(root)
+
+        # BFS
+        while queue:
+            node = queue.popleft()
+            result.append(node.val)
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+        
+        return result
